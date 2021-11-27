ed225fa4c7fb7081d58d0f4690a852d3be479141
ritesh309 <60743868+ritesh309@users.noreply.github.com>
2021-11-27 10:11:14 +0530

    Add files via upload

---
+++ components/Footer.jsx
@@ -0,0 +1,10 @@
+import { Box } from '@chakra-ui/react'
+const Footer = () => {
+    return ( <>
+        <Box textAlign="center" p="5" color="grey" borderTop="1px" borderColor="lightgrey" >
+            2021 Ritesh ©️ Corporation
+        </Box>
+    </> )
+}
+
+export default Footer;
